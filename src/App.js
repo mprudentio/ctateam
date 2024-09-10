052d5e7d23df99bc83c64ae5f8432de0697a4258
mprudentio <muhammad.prudentio@ui.ac.id>
2024-09-10 12:47:18 +0700

    3rd commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       <Helmet>
               <meta charSet="utf-8" />
-              <title>Muhammad Prudentio</title>
+              <title>CTA Team</title>
             </Helmet>
       <Navbar /> 
       <Home />
